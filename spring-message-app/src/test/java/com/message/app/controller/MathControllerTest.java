bd6330ac46d9cdfbca7395bcb51abb0a7d0a796d
Rama Shanker <rama.shanker@wirelesscar.com>
2023-06-05 14:53:20 +0200

    copilot training

---
+++ spring-message-app/src/test/java/com/message/app/controller/MathControllerTest.java
@@ -0,0 +1,12 @@
+package com.message.app.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathControllerTest {
+
+  @Test
+  void fibbonacci() {
+  }
+}
