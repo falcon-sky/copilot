edac18454fcb0cbe7161c982250264bf5d5035cc
Rama Shanker <rama.shanker@wirelesscar.com>
2023-06-05 14:50:42 +0200

    copilot training

--- spring-message-app/src/main/java/com/message/app/controller/MathController.java
+++ spring-message-app/src/main/java/com/message/app/controller/MathController.java
@@ -1,4 +1,18 @@
 package com.message.app.controller;
 
 public class MathController {
+
+  // method to create fibbonacci series
+  public int[] fibbonacci(int n) {
+    int[] fib = new int[n];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+  }
+
+
+
 }
